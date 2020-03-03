dfbd620a809015a77484ca1dfeb0dc9da20d387c
Dophy2706 <dophy2706@gmail.com>
2020-03-03 13:29:46 -0300

    Proceso

---
+++ public_html/js/script.js
@@ -0,0 +1,8 @@
+// Definición de Variables
+
+var canvas = document.getElementById("Canvas");
+console.log(canvas);
+
+var contexto = canvas.getContext('2d');
+var ancho = canvas.width;
+var alto = canvas.height;
