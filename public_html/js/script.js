0c276df589151a7128d56302921ea611d30c2878
Dophy2706 <dophy2706@gmail.com>
2020-03-03 13:47:17 -0300

    Delete File

--- public_html/js/script.js
+++
@@ -1,8 +0,0 @@
-// Definición de Variables
-
-var canvas = document.getElementById("Canvas");
-console.log(canvas);
-
-var contexto = canvas.getContext('2d');
-var ancho = canvas.width;
-var alto = canvas.height;
