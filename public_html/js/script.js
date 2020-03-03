731100f1934e370162ebc9574e9483b48c343fb6
Dophy2706 <dophy2706@gmail.com>
2020-03-03 14:09:43 -0300

    News

---
+++ public_html/js/script.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var canvashtml = document.getElementById("Canvas"); // asignasión del elemento HTML CANVAS a una variable
+var context = canvashtml.getContext("2d"); // creacion de variable context para asignar el contexto de area de diseño ejemplo: 2d
+
+
+
